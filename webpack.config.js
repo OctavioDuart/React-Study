06e52956a048d2ed7dcdbb5cb30131f6fd7f143a
OctavioDuart <36246332+OctavioDuart@users.noreply.github.com>
2018-10-24 18:40:47 -0300

    Setting up environment

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const HtmlWebPackPluginConfig = new HtmlWebPackPlugin({
+    template : './client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }, {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  },
+  plugins: [HtmlWebPackPluginConfig]
+}
